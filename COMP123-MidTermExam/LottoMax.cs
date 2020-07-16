368b08d4c7861766440bb8a426682a562c25663d
Gurvir Singh <gurvirsingh791@gmail.com>
2020-07-16 18:46:16 -0400

    LottoGame abstract class completed with fields, properties and methods. Class Diagram created.

--- COMP123-MidTermExam/LottoMax.cs
+++ COMP123-MidTermExam/LottoMax.cs
@@ -9,7 +9,7 @@ namespace COMP123_MidTermExam
     //STUDENT ID: #301108852
     //COURSE NAME: Programming 2
     //COURSE CODE: SEC.001
-    //MIDTERM TEST
+    //Midterm Test
     /**
     * <summary>
     * This class is a subclass of the LottoGame abstract superclass
