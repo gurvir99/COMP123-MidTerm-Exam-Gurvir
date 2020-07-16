55d6920c34cda9c968cc91719d856a0fecd9f08c
Gurvir Singh <gurvirsingh791@gmail.com>
2020-07-16 18:41:07 -0400

    LottoGame abstract class completed with fields, properties and methods. Class Diagram created.

---
+++ COMP123-MidTermExam/Lotto649.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_MidTermExam
+{
+    //STUDENT NAME: Gurvir Singh
+    //STUDENT ID: #301108852
+    //COURSE NAME: Programming 2
+    //COURSE CODE: SEC.001
+    //MIDTERM TEST
+    /**
+     * <summary>
+     * This class is a subclass of the LottoGame abstract superclass
+     * </summary>
+     * 
+     * @class Lotto649
+     */
+    public class Lotto649 : LottoGame, IGenerateLottoNumbers
+    {
+        // CONSTRUCTORS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This constructor does not take any parameters but satisfies the
+         * base constructor requirements by send the elementNumber and setSize
+         * </summary>
+         * 
+         * @constructor
+         */
+        public Lotto649()
+            : base(6, 49)
+        {
+
+        }
+
+        // CREATE the public GenerateLottoNumbers method here ----------------
+    }
+}
---
+++ COMP123-MidTermExam/LottoMax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_MidTermExam
+{
+    //STUDENT NAME: Gurvir Singh
+    //STUDENT ID: #301108852
+    //COURSE NAME: Programming 2
+    //COURSE CODE: SEC.001
+    //MIDTERM TEST
+    /**
+    * <summary>
+    * This class is a subclass of the LottoGame abstract superclass
+    * </summary>
+    * 
+    * @class LottoMax
+    */
+    public class LottoMax : LottoGame, IGenerateLottoNumbers
+    {
+        /**
+         * <summary>
+         * This constructor does not take any parameters but satisfies the
+         * base constructor requirements by send the elementNumber and setSize
+         * </summary>
+         * 
+         * @constructor
+         */
+        public LottoMax()
+            :base(7,49)
+        {
+            
+        }
+
+        // CREATE the public GenerateLottoNumbers method here ----------------
+    }
+}
