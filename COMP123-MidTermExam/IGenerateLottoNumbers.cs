f1516ed85457342ec46c079e1e59052daed1b529
Gurvir Singh <gurvirsingh791@gmail.com>
2020-07-16 21:23:11 -0400

    IGenerateLottoNumbers interface created.

---
+++ COMP123-MidTermExam/IGenerateLottoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_MidTermExam
+{
+    //STUDENT NAME: Gurvir Singh
+    //STUDENT ID: #301108852
+    //COURSE NAME: Programming 2
+    //COURSE CODE: SEC.001
+    //MIDTERM TEST
+    interface IGenerateLottoNumbers
+    {
+        void GenerateLottoNumbers();
+    }
+}
